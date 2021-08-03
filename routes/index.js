dd1372de36fd933d02aa6ac1be1bf899832f075d
Mabdesaken <mathias.abdesaken@gmail.com>
2021-08-03 15:08:35 +0200

    Proper commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello, World! - I have no clue what Im doing')
+    res.render('index')
 })
 
 module.exports = router
